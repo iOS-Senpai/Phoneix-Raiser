7b58168a877ad11209ccc85827084c0dfb57f890
iOS-Senapi <pradeep.iossenpai@gmail.com>
2023-11-10 07:23:09 +0530

    Initial Commit

---
+++ Phoneix Raiser/Phoneix_RaiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  Phoneix_RaiserApp.swift
+//  Phoneix Raiser
+//
+//  Created by Pradeep's MacBook on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Phoneix_RaiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
